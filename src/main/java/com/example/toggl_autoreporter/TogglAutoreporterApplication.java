3d195c559c7900ec7d9b38f0d5cf41467ecc3bd3
Sergii Lisnychyi <lisnychyis@gmail.com>
2022-08-21 19:30:39 +0200

    Initial commit

---
+++ src/main/java/com/example/toggl_autoreporter/TogglAutoreporterApplication.java
@@ -0,0 +1,13 @@
+package com.example.toggl_autoreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogglAutoreporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogglAutoreporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toggl_autoreporter/TogglAutoreporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toggl_autoreporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogglAutoreporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
